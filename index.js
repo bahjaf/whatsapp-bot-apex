358a29d502b763fea0e308293d2d2482c0e4a4e7
bahjaf <64350237+bahjaf@users.noreply.github.com>
2025-04-28 07:52:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ client.on('ready', () => {
 });
 
 client.initialize();
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is running.');
+}).listen(process.env.PORT || 3000);
